20403bdf1fbfe3fa7d6d482a1d7e9821125351fc
Emmanuel Theodorus <emmantheodorus@gmail.com>
2023-01-30 17:55:36 +1100

    Added weather API to controller code

--- VirtualCoffeeMachine/Controller/CoffeeController.cs
+++ VirtualCoffeeMachine/Controller/CoffeeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+using System;
 using VirtualCoffeeMachineContAPI.Models;
 
 namespace VirtualCoffeeMachineContAPI.Controller
@@ -8,6 +10,10 @@ namespace VirtualCoffeeMachineContAPI.Controller
     public class CoffeeController : ControllerBase
     {
         private readonly CoffeeDb? _coffee;
+
+        //URL for weatherAPI
+        string URL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/22889?";
+        string apiKey = "apikey=yyVx9kMJlA0CJQDYGY8D79trkLNKRnBl";
 
         public CoffeeController(CoffeeDb coffee)
         {
@@ -25,10 +31,25 @@ namespace VirtualCoffeeMachineContAPI.Controller
                 message = "Your piping hot coffee is ready"
             };
 
+            //API integration for weatherAPI
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(URL + apiKey);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            JsonNode weatherForecast = JsonNode.Parse(responseBody)!;
+
+            float temperatureNode = (float)weatherForecast!["DailyForecasts"]![0]!["Temperature"]!["Maximum"]!["Value"];
+            //float temperatureNode = 86f;
+
+            if ((temperatureNode - 32) * 0.5556 > 30)
+            {
+                coffee.message = "Your refreshing iced coffee is ready";
+            }
+
             _coffee.CoffeeQueue.Add(coffee);
             await _coffee.SaveChangesAsync();
 
-            if(_coffee.CoffeeQueue.Count()%5 == 0)
+            if (_coffee.CoffeeQueue.Count()%5 == 0)
             {
                 return StatusCode(503,"");
             }
